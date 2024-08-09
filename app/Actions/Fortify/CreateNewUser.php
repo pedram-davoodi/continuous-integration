c7911eb6808ab4c24cf7348fd6b5de6a8b5717a5
Luke Downing <lukeraymonddowning@gmail.com>
2024-08-09 11:32:39 +0100

    Revert "wip"

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,7 +21,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        // THis is a random comment
+        // THis is a arndom comment
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
