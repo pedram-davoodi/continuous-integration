1914bc509426dc91dfa3f1e19f1a2360fb9ea402
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-08-12 10:45:17 +0100

    wip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -19,17 +19,17 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param array<string, string> $input
      */
-    public function create(array $input): User
-    {
-        // This is a random comment!
+    public function create(array $input): User {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : ''])->validate();
 
-        return DB::transaction(function () use ($input) {
+        return
+
+
+            DB::transaction(function () use ($input) {
             return tap(User::create([
                 'name' => $input['name'],
                 'email' => $input['email'],
