b99d4274f72cad4c063ac44ad01dca555378933e
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-08-12 11:49:57 +0100

    wip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,6 +21,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        // A new comment!
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255', 'nullable'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
